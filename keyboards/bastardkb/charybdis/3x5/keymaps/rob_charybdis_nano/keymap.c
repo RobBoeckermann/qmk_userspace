ffe4b0859969cd7df9205299f690cbb68da720df
RobBoeckermann <robboeckermann@gmail.com>
2024-11-09 13:43:40 -0700

    backup

--- keyboards/bastardkb/charybdis/3x5/keymaps/rob_charybdis_nano/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/rob_charybdis_nano/keymap.c
@@ -18,22 +18,43 @@
 
 
 enum charybdis_keymap_layers {
-    LAYER_BASE = 0,
+    L_BASE = 0,
 };
 
 #define KC_Y_MEDIA LT(L_MEDIA, KC_Y)
+#define KC_J_VSCODE LT(L_VSCODE, KC_J)
+#define KC_ENT_FUNCTION LT(L_FUNCTION, KC_ENT)
+#define KC_BSPC_SHIFT LT(L_SHIFT, KC_BSPC)
+#define KC_TAB_ARROWS LT(L_ARROWS, KC_TAB)
+#define KC_DEL_NUMPAD LT(L_NUMPAD, KC_DEL)
+#define KC_SPC_SYMBOLS LT(L_SYMBOLS, KC_SPC)
+#define KC_V_BLUETOOTH LT(L_BLUETOOTH, KC_SPC)
 
 // clang-format off
-#define LAYOUT_LAYER_BASE                                                                         \
-    KC_Q,               KC_H,               KC_O,                   KC_U,                   KC_X,                 /**/ KC_G,                   KC_C,                KC_R,               KC_F,               KC_Z,                   \
-    KC_Y_MEDIA,         KC_I,               KC_E,                   KC_A,                   KC_DOT,               /**/ KC_D,                   KC_S,                KC_T,               KC_N,               KC_B,                   \
-    LT(L_VSCODE, KC_J), KC_SLSH,            KC_COMM,                KC_K,                   KC_QUOT,              /**/ KC_W,                   KC_M,                KC_L,               KC_P,               LT(L_BLUETOOTH, KC_V),  \
-                                            LT(L_FUNCTION, KC_ENT), LT(L_SHIFT, KC_BSPC),   LT(L_ARROWS, KC_TAB), /**/ LT(L_NUMPAD, KC_DEL),   LT(L_SYMBOLS, KC_SPC)
+#define L_BASE \
+  KC_Q,         KC_H,     KC_O,             KC_U,           KC_X,           /**/  KC_G,           KC_C,           KC_R,   KC_F,   KC_Z,           \
+  KC_Y_MEDIA,   KC_I,     KC_E,             KC_A,           KC_DOT,         /**/  KC_D,           KC_S,           KC_T,   KC_N,   KC_B,           \
+  KC_J_VSCODE,  KC_SLSH,  KC_COMM,          KC_K,           KC_QUOT,        /**/  KC_W,           KC_M,           KC_L,   KC_P,   KC_V_BLUETOOTH, \
+                          KC_ENT_FUNCTION,  KC_BSPC_SHIFT,  KC_TAB_ARROWS,  /**/  KC_DEL_NUMPAD,  KC_SPC_SYMBOLS
 
 /** Convenience row shorthands. */
 #define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 #define ______________HOME_ROW_GACS_L______________ XXXXXXX, KC_LALT, KC_LCTL, KC_LSFT, KC_LGUI
 #define ______________HOME_ROW_GACS_R______________ KC_LGUI, KC_LSFT, KC_LCTL, KC_LALT, XXXXXXX
+
+#define L_SHIFT                                                                          \
+  LSFT(KC_Q), LSFT(KC_H), LSFT(KC_O), LSFT(KC_U), LSFT(KC_X),       LSFT(KC_G), LSFT(KC_C), LSFT(KC_R), LSFT(KC_F), LSFT(KC_Z), \
+  LSFT(KC_Y), LSFT(KC_I), LSFT(KC_E), LSFT(KC_A), KC_AT,            LSFT(KC_D), LSFT(KC_S), LSFT(KC_T), LSFT(KC_N), LSFT(KC_B), \
+  LSFT(KC_J), KC_QUES,    KC_EXLM,    LSFT(KC_K), KC_GRV,           LSFT(KC_W), LSFT(KC_M), LSFT(KC_L), LSFT(KC_P), LSFT(KC_V), \
+                          _______,    _______,    _______,          _______,    _______
+
+#define L_FUNCTION \
+  KC_ESC,            _______,       /* LCTL(LSFT(KC_T)), LCTL(KC_W), */    LCTL(KC_T),  LCTL(KC_S),       KC_F10, KC_F7,    KC_F8, KC_F9,   _______,  _______,  \
+  LCTL(KC_SLSH),     LCTL(KC_Z),    /* LCTL(KC_X),       LCTL(KC_C), */    /* LCTL(KC_V), */  LCTL(LSFT(KC_Z)),       KC_F11, _______,  KC_F1, KC_F2,   KC_F3,    _______,  \
+LALT(LSFT(KC_A)),  LCTL(KC_F),    LCTL(LSFT(KC_F)), _______,       _______,                               KC_F12, KC_F4,    KC_F5,    KC_F6, _______, _______,            \
+                                      _______,        _______,       _______,     _______,        _______
+
+
 
 
 /**
@@ -61,6 +82,6 @@ enum charybdis_keymap_layers {
 #define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [LAYER_BASE] = LAYOUT_wrapper(HOME_ROW_MOD_GACS(LAYOUT_LAYER_BASE))
+  [LAYER_BASE] = LAYOUT_wrapper(HOME_ROW_MOD_GACS(L_BASE))
 };
 // clang-format on
