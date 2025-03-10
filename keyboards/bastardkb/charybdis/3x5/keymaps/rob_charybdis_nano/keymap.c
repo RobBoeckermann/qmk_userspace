ff0960180003dad4aec0dfaf7ebfc99e0172d3eb
RobBoeckermann <robboeckermann@gmail.com>
2025-03-09 20:10:17 -0600

    Implement QMK pointer functionality instead of using BK.
    bk implementation wasn't working and wasn't documented well enough for me to troubleshoot.
    remove bk configs and keycodes in favor of qmk equivalents.
    
    rules.mk pointer rotation or inversions not working.
    CPI/DPI customization not working.
    drag scroll overly sensitive and disables while still holding the key.

--- keyboards/bastardkb/charybdis/3x5/keymaps/rob_charybdis_nano/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/rob_charybdis_nano/keymap.c
@@ -18,10 +18,6 @@
 #include "custom_keycodes.h"
 #include "process_tap_dance.h"
 
-#ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-#    include "timer.h"
-#endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-
 // TODO:
 // DPI configs and buttons https://docs.bastardkb.com/fw/charybdis-features.html
 // Dynamic macros https://docs.qmk.fm/keycodes#dynamic-macros
@@ -67,20 +63,10 @@ enum charybdis_keymap_layers {
     L_POINTER,
 };
 
-// Automatically enable sniping-mode on the pointer layer.
-#define CHARYBDIS_AUTO_SNIPING_ON_LAYER L_POINTER
-
-#ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-static uint16_t auto_pointer_layer_timer = 0;
-
-#    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
-#        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 1000
-#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
-
-#    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
-#        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 8
-#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
-#endif     // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+void pointing_device_init_user(void) {
+    set_auto_mouse_layer(L_POINTER); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
+    set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
+}
 
 #define _L_PTR(KC) LT(L_POINTER, KC)
 
@@ -208,10 +194,10 @@ _______,    _______,    _______,    _______,    _______, /**/ _______,  _______,
 // |    |    |    |    |    | /**/ |    |    |    |    |    |
 //           |    |    |    | /**/ |    |    |
 #define LAYER_POINTER \
-_______,    _______,    _______,    _______,    _______, /**/ S_D_MOD,          _______,    _______,    _______,  DPI_MOD, \
-_______,    KC_BTN3,    KC_BTN1,    KC_BTN2,    _______, /**/ KC_SNIPING_RESET, KC_BTN2,    KC_BTN1,    KC_BTN3,  KC_DPI_RESET, \
-_______,    _______,    _______,    _______,    _______, /**/ S_D_RMOD,          _______,   _______,    _______,  DPI_RMOD, \
-                        DRGSCRL,    SNIPING,    _______, /**/ _______,  _______
+_______,    _______,                  _______,            _______,            _______, /**/ S_D_MOD,          _______,            _______,            _______,            CPI_UP, \
+_______,    QK_MOUSE_BUTTON_3,        QK_MOUSE_BUTTON_2,  QK_MOUSE_BUTTON_1,  _______, /**/ KC_SNIPING_RESET, QK_MOUSE_BUTTON_1,  QK_MOUSE_BUTTON_2,  QK_MOUSE_BUTTON_3,  CPI_RESET, \
+_______,    _______,                  _______,            _______,            _______, /**/ S_D_RMOD,         _______,           _______,            _______,             CPI_DOWN, \
+                                      _______,            DRAG_SCROLL,        _______, /**/ _______,          _______
 
 //TODO: SNIPING is locking on. is this due to the auto layer disabling while the button is still pressed? 
 //edit the keycode to unlock if the auto layer disables.
@@ -396,31 +382,3 @@ tap_dance_action_t tap_dance_actions[] = {
 };
 
 // clang-format on
-
-#ifdef POINTING_DEVICE_ENABLE
-#    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
-        if (auto_pointer_layer_timer == 0) {
-            layer_on(L_POINTER);
-        }
-        auto_pointer_layer_timer = timer_read();
-    }
-    return mouse_report;
-}
-
-void matrix_scan_user(void) {
-    if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
-        auto_pointer_layer_timer = 0;
-        layer_off(L_POINTER);
-    }
-}
-#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-
-#    ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
-layer_state_t layer_state_set_user(layer_state_t state) {
-    charybdis_set_pointer_sniping_enabled(layer_state_cmp(state, CHARYBDIS_AUTO_SNIPING_ON_LAYER));
-    return state;
-}
-#    endif // CHARYBDIS_AUTO_SNIPING_ON_LAYER
-#endif     // POINTING_DEVICE_ENABLE
